a063e8833346a5467d70d8aa811401bed071aba2
Ivan Vodzich <IVodzich@iba.by>
2017-08-17 10:11:11 +0300

    starte the registration page

---
+++ src/components/registration-page/index.tsx
@@ -0,0 +1,9 @@
+import * as React from "react";
+
+export class RegistrationPage extends React.Component {
+    render() {
+        return <main>
+            <div className="registration-page-container">There will be the registration page</div>
+        </main>
+    }
+}
