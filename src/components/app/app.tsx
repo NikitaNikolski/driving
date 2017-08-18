c934949a06f3c0af002bc7e9702bf5fec9b32d3f
pylis <egenpark@gmail.com>
2017-08-18 15:57:07 +0300

    Added menuItem component

--- src/components/app/app.tsx
+++ src/components/app/app.tsx
@@ -2,11 +2,13 @@ import * as React from "react";
 
 import Header from "../header";
 import MainPage from "../main-page";
+import MenuItem from "../menuItem";
 
 export default class App extends React.Component {
     render() {
         return <div className="app-container">
             <Header />
+            <MenuItem pictureUrl="src/images/person-3.svg" buttonText="bla" />
             <MainPage />
         </div>
     }
